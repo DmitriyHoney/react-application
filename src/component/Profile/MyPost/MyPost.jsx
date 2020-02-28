b5734171b120bc0f025c10a00dedab80300aeea2
Bolshakov Dmitry <vanove2013@yandex.ru>
2020-02-28 23:05:41 +0200

    Урок 24 Вывел посты, а также участников диалога с помощью метода map

--- src/component/Profile/MyPost/MyPost.jsx
+++ src/component/Profile/MyPost/MyPost.jsx
@@ -4,13 +4,21 @@ import Post from "./Post/Post";
 import SubmitPost from "./SubmitPost/SubmitPost";
 
 
-function MyPost() {
+const MyPost = () => {
+    let messageData = [
+        {message: "My first post! Hello, world!", likeCount: 7},
+        {message: "You don`t know me, but believe me", likeCount: 25},
+        {message: "В частности, сплочённость команды профессионалов представляет собой интересный эксперимент проверки существующих финансовых и административных условий.", likeCount: 42},
+    ];
+
+    let renderMessageData = messageData.map( (post) => {
+        return <Post message={post.message} likeCount={post.likeCount}/>
+    });
+
     return(
         <div className={style.postWrap}>
             <SubmitPost/>
-            <Post message="My first post! Hello, world!" likeCount="7"/>
-            <Post message="You don`t know me, but believe me" likeCount="25"/>
-            <Post message="В частности, сплочённость команды профессионалов представляет собой интересный эксперимент проверки существующих финансовых и административных условий." likeCount="42" />
+            {renderMessageData}
         </div>
     )
 }
